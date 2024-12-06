e80eb9b21afca53791dfcc049584b201198bd7ab
椎名潤七 <newbingtochatgpt233@outlook.com>
2024-12-06 16:15:23 +0800

    666

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,89 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+function showTip() {
+  alert('你好')
+}
 </script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div @click="showTip" class="todo-button">Click me!</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.todo-app {
+  box-sizing: border-box;
+  margin-top: 40px;
+  margin-left: 1%;
+  padding-top: 30px;
+  width: 98%;
+  height: 500px;
+  background: #ffffff;
+  border-radius: 5px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.title {
+  text-align: center;
+  font-size: 30px;
+  font-weight: 700;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.todo-form {
+  display: flex;
+  margin: 20px 0 30px 20px;
+}
+
+.todo-button {
+  width: 100px;
+  height: 52px;
+  border-radius: 0 20px 20px 0;
+
+  text-align: center;
+  background: linear-gradient(to right,
+      rgb(113, 65, 168),
+      rgba(44, 114, 251, 1));
+  color: #fff;
+  line-height: 52px;
+  cursor: pointer;
+  font-size: 14px;
+  user-select: none;
+}
+
+.todo-input {
+  padding: 0px 15px 0px 15px;
+  border-radius: 20px 0 0 20px;
+  border: 1px solid #dfe1e5;
+  outline: none;
+  width: 60%;
+  height: 50px;
+}
+
+.item {
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 80%;
+  height: 50px;
+  margin: 8px auto;
+  padding: 16px;
+  border-radius: 20px;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 20px;
+}
+
+.del {
+  color: red;
+}
+
+.completed {
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 80%;
+  height: 50px;
+  margin: 8px auto;
+  padding: 16px;
+  border-radius: 20px;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 20px;
+  text-decoration: line-through;
+  opacity: 0.4;
 }
 </style>
